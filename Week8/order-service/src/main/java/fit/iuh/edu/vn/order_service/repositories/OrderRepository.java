734ef330df6dfa835c3621e116e1bbbea186da39
hiepgit2812 <21081841.hiep@student.iuh.edu.vn>
2025-04-25 17:48:06 +0700

    Week3

--- Week8/order-service/src/main/java/fit/iuh/edu/vn/order_service/repositories/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package fit.iuh.edu.vn.order_service.repositories;
-
-import fit.iuh.edu.vn.order_service.entities.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-    // Custom query methods can be defined here if needed
-    // For example, findByCustomerId, findByStatus, etc.
-}
